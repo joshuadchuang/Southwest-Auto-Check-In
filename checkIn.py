da5de5caaf3d5530c147507002649a60d93fa1af
Joshua Chuang <jchuang77@berkeley.edu>
2023-08-05 14:52:18 -0700

    added functionality for pop-up UI and scheduler for auto-script

--- checkIn.py
+++ checkIn.py
@@ -1,29 +1,62 @@
-#!/usr/bin/env python3
-
+from apscheduler.schedulers.blocking import BlockingScheduler
 from selenium import webdriver
-from selenium.webdriver.common.keys import Keys
-import time
+from selenium.webdriver.common.by import By
+import tkinter as tk
+from tkinter import simpledialog
+from tkinter import messagebox
+from datetime import datetime, timedelta
 
-driver = webdriver.Chrome()
-driver.get("https://www.southwest.com/air/check-in/index.html")
+root = tk.Tk()
+root.withdraw()
+messagebox.showinfo("Title", "Hello World")
 
-# TODO: Input your information here!! ------------------------------------------------------------------------
-confirmation_number = "your_confirmation_number"
-first_name = "your_first_name"
-last_name = "your_last_name"
-# ------------------------------------------------------------------------------------------------------------
+def get_user_info():
+    # Create a new tkinter window
+    root = tk.Tk()
+    # Hide the main window
+    root.withdraw()
+    
+    first_name = simpledialog.askstring("Input", "Enter your first name",
+                                        parent=root)
+    last_name = simpledialog.askstring("Input", "Enter your last name",
+                                       parent=root)
+    confirmation_number = simpledialog.askstring("Input", "Enter your confirmation number",
+                                                 parent=root)
+    # Get check-in date and time
+    check_in_datetime = simpledialog.askstring("Input", "Enter the date and time for check-in (format: 'YYYY-MM-DD HH:MM')",
+                                               parent=root)
 
-# Login to site using credentials
-confirmation_input = driver.find_element_by_id("confirmationNumber")
-confirmation_input.send_keys(confirmation_number)
+    # Convert check-in date and time to datetime object
+    check_in_datetime = datetime.strptime(check_in_datetime, '%Y-%m-%d %H:%M')
 
-first_name_input = driver.find_element_by_id("passengerFirstName")
-first_name_input.send_keys(first_name)
+    # Add 5 seconds to the check-in time
+    check_in_datetime = check_in_datetime + timedelta(seconds=5)
 
-last_name_input = driver.find_element_by_id("passengerLastName")
-last_name_input.send_keys(last_name)
+    return first_name, last_name, confirmation_number, check_in_datetime
 
-check_in_button = driver.find_element_by_id("form-mixin--submit-button")
-check_in_button.click()
+def check_in():
+    first_name, last_name, confirmation_number, check_in_datetime = get_user_info()
+    driver = webdriver.Chrome()
+    driver.get("https://www.southwest.com/air/check-in/index.html")
 
-# Handle MFA
+    # Login to site using credentials
+    confirmation_input = driver.find_element(By.ID, "confirmationNumber")
+    first_name_input = driver.find_element(By.ID, "passengerFirstName")
+    last_name_input = driver.find_element(By.ID, "passengerLastName")
+    check_in_button = driver.find_element(By.ID, "form-mixin--submit-button")
+
+    confirmation_input.send_keys(confirmation_number)
+    first_name_input.send_keys(first_name)
+    last_name_input.send_keys(last_name)
+
+    check_in_button.click()
+
+    # Handle MFA
+    # Handling MFA would depend on the specific implementation.
+
+scheduler = BlockingScheduler()
+# Get user info and check-in date and time
+first_name, last_name, confirmation_number, check_in_datetime = get_user_info()
+# Schedule the check-in job
+scheduler.add_job(check_in, 'date', run_date=check_in_datetime) 
+scheduler.start()
